e074efba09a9e262549ca2a633a2b837991f6d55
Arthur Allilaire <arthur.allilaire@gmail.com>
2021-06-30 12:30:53 +0100

    Finished project0b, tictactoe

---
+++ Project 0/tictactoe/tictactoe.py
@@ -0,0 +1,202 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    # Count number of X's and number of O's
+    num_x = 0
+    num_o = 0
+    for col in board:
+        for cell in col:
+            if cell == X:
+                num_x += 1
+            elif cell == O:
+                num_o += 1
+
+    # If the board is empty
+    if num_x == 0 and num_o == 0:
+        return X
+    elif num_x > num_o:
+        return O
+    elif num_x == num_o:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    actions = set()
+    for row in range(3):
+        for col in range(3):
+            cell = board[row][col]
+            if cell == EMPTY:
+                actions.add((row, col))
+    return actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    # Copy the board
+    new_board = copy.deepcopy(board)
+
+    # check if the action is legal
+    if board[action[0]][action[1]] != EMPTY:
+        raise Exception("The action is not a legal move")
+
+    # Get the move of the current player
+    new_board[action[0]][action[1]] = player(board)
+
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    def check_row(row):
+        """ 
+        Takes a list with 3 cells and returns X if all cells have X, O if all have O and None otherwise
+        """
+        x_count = 0
+        o_count = 0
+        for cell in row:
+            if cell == X:
+                x_count += 1
+            elif cell == O:
+                o_count += 1
+        if x_count == 3:
+            return X
+        elif o_count == 3:
+            return O
+        else:
+            return None
+
+    # Check the rows to see if they have a 3 in a row
+    for row in board:
+        result = check_row(row)
+        # If the row contained 3 in a row return winner else do nothing
+        if result:
+            return result
+
+    # check the columns
+    for i in range(3):
+        col = []
+        # For each row in the colum append the righ row
+        for row in board:
+            col.append(row[i])
+        # If the column contained 3 in a row return winner else do nothing
+        result = check_row(col)
+        if result:
+            return result
+
+    # Check the diagonals
+    diagonalL = [board[0][0], board[1][1], board[2][2]]
+    result = check_row(diagonalL)
+    if result:
+        return result
+    diagonalR = [board[0][2], board[1][1], board[2][0]]
+    result = check_row(diagonalR)
+    if result:
+        return result
+
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    # If there is a winner return True
+    if winner(board):
+        print("Winner:", winner(board))
+        return True
+    # If there are still empty spaces return False, can still add
+    for row in board:
+        for cell in row:
+            if cell == None:
+                return False
+
+    # else return True
+    return True
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    # If game ended return None
+    if terminal(board):
+        print("Game ended")
+        return None
+
+    def recursive_minimax(board):
+        """ best_actions is a list that will go through recursion, can be added from anywhere. Returns the list of optimal actions the player should take."""
+        turn = player(board)
+        if turn == O:
+            best = float("inf")
+        else:
+            best = float("-inf")
+        # Base case
+        if terminal(board):
+            # No more actions to take, return end utility of board
+            return (None, utility(board))
+        # Get all the possible actions, uses depth first search
+        for action in actions(board):
+            # print("action", action)
+            # print("Result: ", result(board, action))
+            path = recursive_minimax(
+                result(board, action)
+            )
+            if turn == O and path[1] < best:
+                best = path[1]
+                best_action = action
+            elif turn == X and path[1] > best:
+                best = path[1]
+                best_action = action
+
+        return (best_action, best)
+
+    # Else Return the best solution found using recursive function, which is the first in the tuple returned
+    return recursive_minimax(board)[0]
+
+
+if __name__ == '__main__':
+    initial = initial_state()
+
+    print(minimax(initial))
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # CS50-Intro-to-AI
+## Project 0
+### Degrees
+#### Specification
+Complete the implementation of the shortest_path function such that it returns the shortest path from the person with id source to the person with the id target.
+*   Assuming there is a path from the `source` to the `target`, your function should return a list, where each list item is the next `(movie_id, person_id)` pair in the path from the source to the target. Each pair should be a tuple of two `int`s.
+    *   For example, if the return value of `shortest_path` were `[(1, 2), (3, 4)]`, that would mean that the source starred in movie 1 with person 2, person 2 starred in movie 3 with person 4, and person 4 is the target.
+*   If there are multiple paths of minimum length from the source to the target, your function can return any of them.
+*   If there is no possible path between two actors, your function should return `None`.
+*   You may call the `neighbors_for_person` function, which accepts a person’s id as input, and returns a set of `(movie_id, person_id)` pairs for all people who starred in a movie with a given person.
+#### Time scale: 2-5 hours
+#### Comments: 
+For testing, a good pair to use is tom Hanks to Dustin Hoffman, the output should be:
+3 degrees of separation.
+1: Tom Hanks and Kevin Bacon starred in Apollo 13
+2: Kevin Bacon and Tom Cruise starred in A Few Good Men
+3: Tom Cruise and Dustin Hoffman starred in Rain Man
+Which is represented by this list of tuples:
+Path:  [('95953', '163'), ('104257', '129'), ('112384', '102')]
+
+
